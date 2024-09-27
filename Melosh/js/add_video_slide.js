79a4da63a4f9405437c7386e36aa4a95b2148a43
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 20:25:04 +0000

    Added the tissue expansion fibrosis paper [ci skip]

--- Melosh/js/add_video_slide.js
+++ Melosh/js/add_video_slide.js
@@ -1,18 +1,1 @@
-var slide = document.currentScript.getAttribute('slide_scene');
-function loadFile(filePath) {
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", filePath, false);
-  xmlhttp.send();
-  if (xmlhttp.status==200) {
-    result = xmlhttp.responseText;
-  }
-  console.log("load")
-  return result;
-}
-frags = loadFile(`video_slides/${slide}.txt`)
-
-document.write(`
-<section data-background-video="./video_slides/${slide}.mp4" data-background-color="#000000" data-background-size="contain" id="vid" type="videoslide">
- ${frags}
-</section>`)
+function loadFile(e){var t=null,d=new XMLHttpRequest;return d.open("GET",e,!1),d.send(),200==d.status&&(t=d.responseText),console.log("load"),t}var slide=document.currentScript.getAttribute("slide_scene");frags=loadFile(`video_slides/${slide}.txt`),document.write(`\n<section data-background-video="./video_slides/${slide}.mp4" data-background-color="#000000" data-background-size="contain" id="vid" type="videoslide">\n ${frags}\n</section>`);
--- _news/oigp_spring_reception.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-date: 2023-05-03 10:00:00-0400
-inline: true
----
-
-I presented a [poster]({{ site.url }}/assets/pdf/OIGP_poster.pdf) at the 2023 <a href="https://www.purdue.edu/gradschool/oigp/events/spring-reception/index.html">Spring Reception</a> of the Office of Interdisciplinary Graduate Programs at Purdue.
